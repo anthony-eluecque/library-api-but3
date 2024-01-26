da8951811cd6361e2e85975ae9ff1d54930eabab
Antorakk <95444282+Antorakk@users.noreply.github.com>
2024-01-26 11:04:18 +0100

    Feat: Added CatalogService

---
+++ library-app/Services/Services/CatalogService.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        public void FindBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowCatalog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
