a7e9a73625581328e234d33d903f07f4629cdacf
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2024-01-30 11:04:20 +0100

    Feat: Unit test for FindBook()

--- library-app/Services.Test/CatalogServiceTest.cs
+++ library-app/Services.Test/CatalogServiceTest.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Data;
 using Services.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using BusinessObjects.Entity;
 
 namespace Services.Test
 {
@@ -24,7 +25,22 @@ namespace Services.Test
             // Assert
             Mock.Get(catalogManagerMock).Verify(cm => cm.DisplayCatalog(), Times.Once);
         }
-        
-        
+
+
+        [TestMethod]
+        public async Task FindBook_Should_Call_FindBook()
+        {
+            var catalogManagerMock = Mock.Of<ICatalogManager>();
+            var catalogService = new CatalogService(catalogManagerMock);
+
+            // Set up the behavior for FindBook method
+            Mock.Get(catalogManagerMock).Setup(cm => cm.FindBook(It.IsAny<int>())).Returns(Task.FromResult(new Book()));
+
+            // Act
+            await catalogService.FindBook(1);
+
+            // Assert
+            Mock.Get(catalogManagerMock).Verify(cm => cm.FindBook(It.IsAny<int>()), Times.Once);
+        }
     }
 }
