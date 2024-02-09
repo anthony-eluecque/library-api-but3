5ca88fa908e065026e49fb209fb70a62cb432e4c
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2024-02-09 17:03:07 +0100

    Feat: Final tests for CatalogManager

--- library-app/BusinessLayer.Test/CatalogManagerTests.cs
+++ library-app/BusinessLayer.Test/CatalogManagerTests.cs
@@ -72,6 +72,67 @@ namespace BusinessLayer.Test
             Assert.AreEqual(5, _book.First().Rate);
         }
 
+        [TestMethod]
+        public async Task GetBooks_Should_Call_GetAll()
+        {
+            _catalogManagerMock.Setup(x => x.GetAll()).Returns(Task.FromResult<List<Book>>(_book.ToList()));
+
+            // Act
+            Task task = _catalogManager.GetBooks();
+
+            // Assert
+            Assert.AreEqual(2, _book.Count());
+        }
+
+        [TestMethod]
+        public async Task GetBooksByType_Should_Call_GetBooksByType()
+        {
+            _catalogManagerMock.Setup(x => x.GetAll()).Returns(Task.FromResult<List<Book>>(_book.ToList()));
+
+            // Act
+            Task task = _catalogManager.GetBooksByType("FANTASY");
+
+            // Assert
+            Assert.AreEqual(2, _book.Count());
+        }
+
+        [TestMethod]
+        public async Task UpdateBook_Should_Call_Update()
+        {
+            _catalogManagerMock.Setup(x => x.Update(It.IsAny<Book>())).Returns(Task.FromResult(true));
+
+            // Act
+            Task task = _catalogManager.UpdateBook(new Book());
+
+            // Assert
+            _catalogManagerMock.Verify(x => x.Update(It.IsAny<Book>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task DeleteBook_Should_Call_Delete()
+        {
+            // on setup le mock avec l'id 1
+            _catalogManagerMock.Setup(x => x.Get(1)).Returns(Task.FromResult(_book.First()));
+
+            // Act
+            Task task = _catalogManager.DeleteBook(1);
+
+            // Assert
+            _catalogManagerMock.Verify(x => x.Delete(_book.First()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task AddBook_Should_Call_Add()
+        {
+            _catalogManagerMock.Setup(x => x.Add(It.IsAny<Book>())).Returns(Task.FromResult(true));
+
+            // Act
+            Task task = _catalogManager.AddBook(new Book());
+
+            // Assert
+            _catalogManagerMock.Verify(x => x.Add(It.IsAny<Book>()), Times.Once);
+        }
+
         private IEnumerable<Book> CreateFakeData()
         {
             var books = new List<Book>
