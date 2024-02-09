bcf3f040126db607f1631ea2e3aec754ebd3f521
Antorakk <95444282+Antorakk@users.noreply.github.com>
2024-02-09 09:52:36 +0100

    Feat: Added Book Controller
    GetById method

---
+++ library-app/LibraryManager.Hosting/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObjects.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+
+namespace LibraryManager.Hosting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : Controller
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ICatalogService _catalogService;
+
+        public BookController(ILogger<WeatherForecastController> logger, ICatalogService catalogService)
+        {
+            _logger = logger;
+            _catalogService = catalogService;
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Book>> Get(int id)
+        {
+            return await _catalogService.FindBook(id);
+        }
+    }
+}
