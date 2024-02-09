bd7271d1bfd3f47f7777e92b83d9c4d94ee7aecd
Antorakk <95444282+Antorakk@users.noreply.github.com>
2024-02-09 09:21:34 +0100

    Feat: Added initial Configuration
    Will be rework soon

--- library-app/LibraryManager.Hosting/Program.cs
+++ library-app/LibraryManager.Hosting/Program.cs
@@ -1,25 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using BusinessLayer.Catalog;
+using BusinessObjects.Entity;
+using DataAccessLayer.Data;
+using DataAccessLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+using BusinessLayer.Catalog;
+using BusinessObjects.Entity;
+using DataAccessLayer.Data;
+using DataAccessLayer.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Services.Services;
+using System.Configuration;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddScoped<ICatalogService, CatalogService>();
+        builder.Services.AddScoped<ICatalogManager, CatalogManager>();
+        builder.Services.AddScoped<IRepository<Book>, BookRepository>();
+
+        builder.Services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
